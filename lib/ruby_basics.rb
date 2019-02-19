595d6604aeeae7d989799622cb3ace3044945d1f
Audra Tallman <audra.tallman@hotmail.com>
2019-02-19 23:47:46 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I am right and you are wrong"
 end
 
-def greeting
-
+def greeting(greeting, name)
+  "#{greeting},#{name}."
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
+  puts "The pizza party will have #{topping}."
+end
 
+def topping(pizza = "pepperoni")
+ puts topping = #{pizza}
 end
